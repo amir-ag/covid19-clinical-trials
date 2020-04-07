5bdeae1dde774590b5c4837e6257e0f0a8134573
amir_g <info@ghani.ch>
2020-04-07 16:45:13 +0200

    add backend

---
+++ backend/project/studies/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Study(models.Model):
+
+    BriefTitle = models.TextField(verbose_name="title")
+    BriefSummary = models.TextField(verbose_name="summary")
+    OverallStatus = models.TextField(verbose_name="status")
+    CentralContactName = models.TextField(verbose_name="contact-name")
+    CentralContactEMail = models.TextField(verbose_name="contact-email")
+    CentralContactPhone = models.TextField(verbose_name="contact-phone")
+    LocationFacility = models.TextField(verbose_name="facility")
+    LocationState = models.TextField(verbose_name="state")
+    LocationZip = models.TextField(verbose_name="zip-code")
+    LocationCountry = models.TextField(verbose_name="country")
+
+    def __str__(self):
+        return self.BriefTitle
+
+"https://clinicaltrials.gov/api/query/study_fields?expr=COVID-19&fields=BriefTitle%2CBriefSummary%2COverallStatus%2CCentralContactEMail%2CCentralContactName%2CCentralContactPhone%2CLocationFacility%2CLocationCity%2CLocationState%2CLocationZip%2CLocationCountry&min_rnk=1&max_rnk=1000&fmt=json"
