4db9f05c03532062c4bd747c0a36daee847a1cf9
amir_g <info@ghani.ch>
2020-04-07 18:39:06 +0200

    fix import_export issue

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'import_export',
 
     'project.users',
     'project.studies',
-    'project.import_export',
 ]
 
 MIDDLEWARE = [
