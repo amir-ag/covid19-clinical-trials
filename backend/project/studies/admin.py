013ea56613b1f0daf20cae3913604b607529b887
amir_g <info@ghani.ch>
2020-04-08 14:38:03 +0200

    add fields to study model, add functionality to create link

--- backend/project/studies/admin.py
+++ backend/project/studies/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
-# Register your models here.
 from .models import Study
+
 
 @admin.register(Study)
 class StudyAdmin(ImportExportModelAdmin):
-    pass
+
+    @staticmethod
+    def Link(self, obj):
+        if obj.NCTId:
+            return "<a href='%s'>Link</a>" % "https://clinicaltrials.gov/ct2/show/" + obj.NCTId
+        else:
+            return ''
