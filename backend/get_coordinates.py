bc3bb8ff9fbe5e7ea69c2379568adceecad3766e
amir_g <info@ghani.ch>
2020-04-15 15:04:32 +0200

    add python script to get studies add python script to get coordinates searching for studyFacility

--- backend/get_coordinates.py
+++ backend/get_coordinates.py
@@ -1,6 +1,26 @@
+import os
+import django
 from geopy.geocoders import GoogleV3
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+django.setup()
 
-geolocator = GoogleV3(api_key="AIzaSyAXHJ8U6Jq865DxZqti6wQsLRp7IkE8uSg", user_agent="dashboard")
-location = geolocator.geocode("ISGlobal")
-print(location.latitude, location.longitude)
+from project.studies.models import Study
+
+# geolocator = GoogleV3(api_key="AIzaSyCTnTT4eg4Qjz7JA0BL8l7JjxFxQvhpw-s")
+# location = geolocator.geocode("ISGlobal")
+# print(location.latitude, location.longitude)
+
+
+studies = Study.objects.values()
+for study in studies[:1]:
+    if not study["Latitude"]:
+        search_term = f'{study["LocationFacility"][0]}'
+        print(search_term)
+        # geolocator = GoogleV3(api_key="AIzaSyCTnTT4eg4Qjz7JA0BL8l7JjxFxQvhpw-s")
+        # location = geolocator.geocode(f'{search_term}')
+        entry = Study.objects.get(NCTId=study['NCTId'])
+        print(study['NCTId'])
+        # entry.Latitude = location[0]
+        # entry.Longitude = location[1]
+        # entry.save()
