4b76a3f378c0bc57504bfba66f7360c76fb2ba8b
amir_g <info@ghani.ch>
2020-04-19 19:53:39 +0200

    revised scripts to automatically run at night (gets studies, add location to studies) add syntax to clean double db entries split each study up in locations, one db entry for each location in study

--- backend/get_studies.py
+++ backend/get_studies.py
@@ -4,52 +4,69 @@ import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 django.setup()
-
+import schedule
+import time
 from project.studies.models import Study
 studies = Study.objects.values()
 
-url = "https://clinicaltrials.gov/api/query/study_fields?expr=COVID-19&fields=NCTId%2CBriefTitle%2CBriefSummary%2COverallStatus%2CCentralContactEMail%2CCentralContactName%2CCentralContactPhone%2CLocationFacility%2CLocationCity%2CLocationState%2CLocationZip%2CLocationCountry%2CInterventionDescription%2CInterventionName&min_rnk=1&max_rnk=1000&fmt=json"
+url = "https://clinicaltrials.gov/api/query/study_fields?expr=COVID-19&fields=NCTId%2CBriefTitle%2CBriefSummary%2COverallStatus%2CCentralContactEMail%2CCentralContactName%2CCentralContactPhone%2CLocationFacility%2CLeadSponsorName%2CLocationCity%2CLocationState%2CLocationZip%2CLocationCountry%2CInterventionDescription%2CInterventionName&min_rnk=1&max_rnk=1000&fmt=json"
 r = requests.get(url)
 results = r.json()
 
 
-count = 0
-for entry in results["StudyFieldsResponse"]["StudyFields"]:
-    try:
-        obj = Study.objects.get(NCTId=entry['NCTId'][0])
-    except Study.DoesNotExist:
-        print("does not exist - create new!")
-        count += 1
-        obj = Study(
-            NCTId=entry['NCTId'][0],
-            BriefTitle=entry['BriefTitle'][0],
-            BriefSummary=entry['BriefSummary'][0],
-            InterventionDescription=str(entry['InterventionDescription']),
-            InterventionName=str(entry['InterventionName']),
-            OverallStatus=str(entry['OverallStatus']),
-            CentralContactName=entry['CentralContactName'],
-            CentralContactEMail=entry['CentralContactEMail'],
-            CentralContactPhone=entry['CentralContactPhone'],
-            LocationFacility=entry['LocationFacility'],
-            LocationCity=entry['LocationCity'],
-            LocationState=entry['LocationState'],
-            LocationZip=entry['LocationZip'],
-            LocationCountry=entry['LocationCountry']
-        )
-        obj.save()
-print(count)
+def get_studies():
+    count = 0
+    for entry in results["StudyFieldsResponse"]["StudyFields"]:
+        try:
+            obj = Study.objects.get(NCTId=entry['NCTId'][0])
+        except Study.DoesNotExist:
+            for location in range(len(entry['LocationFacility'])):
+                print("does not exist - create new!")
+                try:
+                    loc_LocationState = entry['LocationState'][location]
+                except:
+                    loc_LocationState = None
 
+                try:
+                    loc_LocationZip = entry['LocationZip'][location]
+                except:
+                    loc_LocationZip = None
 
+                count += 1
+                obj = Study(
+                    NCTId=entry['NCTId'][0],
+                    BriefTitle=entry['BriefTitle'][0],
+                    BriefSummary=entry['BriefSummary'][0],
+                    InterventionDescription=entry['InterventionDescription'],
+                    InterventionName=entry['InterventionName'],
+                    OverallStatus=entry['OverallStatus'][0],
+                    CentralContactName=entry['CentralContactName'],
+                    CentralContactEMail=entry['CentralContactEMail'],
+                    CentralContactPhone=entry['CentralContactPhone'],
+                    LeadSponsorName=entry['LeadSponsorName'],
+                    LocationFacility=entry['LocationFacility'][location],
+                    LocationCity=entry['LocationCity'][location],
+                    LocationState=f'{loc_LocationState}',
+                    LocationZip=f'{loc_LocationZip}',
+                    LocationCountry=entry['LocationCountry'][location]
+                )
+                obj.save()
+        print(count)
 
+    #clean up db for double occurences
+    delete_count = 0
+    for row in Study.objects.all().reverse():
+        if Study.objects.filter(NCTId=row.NCTId, LocationFacility=row.LocationFacility, LocationCountry=row.LocationCountry, LocationZip=row.LocationZip, LocationCity=row.LocationCity, LocationState=row.LocationState,  ).count() > 1:
+            print(row.NCTId)
+            delete_count += 1
+            row.delete()
+    print('delete_count:', delete_count)
 
+# if __name__ == '__main__':
+#     get_studies()
 
+schedule.every().day.at("02:20").do(get_studies)
 
-#     if Study.objects.filter(NCTId=entry["NCTId"]).exists():
-#         print(entry["NCTId"], "exists!")
-#         count += 1
-# print(count)
-
-#     if entry["NCTId"][0] == study["NCTId"]:
-# for study in studies:
-# print(study["NCTId"], "found!")
-
+while True:
+    schedule.run_pending()
+    time.sleep(1)
--- scripts/run.sh
+++ scripts/run.sh
@@ -6,10 +6,10 @@ rm -rf /frontend/build/* && cp -r /frontend_tmp/* /frontend
 python manage.py migrate
 python manage.py collectstatic --no-input
 
+python get_studies.py
+python get_coordinates.py
 #python manage.py runserver 0.0.0.0:8000  # don't use in prod: https://vsupalov.com/django-runserver-in-production/
 #/opt/conda/envs/app/bin/gunicorn -w 4 -b 0.0.0.0:8000 app.wsgi:application
 gunicorn -w 4 -b 0.0.0.0:8000 project.wsgi:application
-
-
 # -w stands for workers
 # -b stands for bind
