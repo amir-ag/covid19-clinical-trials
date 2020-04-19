af9ef687c457965c1b2f6e8915277d42e9160166
amir_g <info@ghani.ch>
2020-04-19 21:51:03 +0200

    add makemigrations

--- backend/project/studies/admin.py
+++ backend/project/studies/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 from .models import Study
-#
+
 
 @admin.register(Study)
 class StudyAdmin(ImportExportModelAdmin):
